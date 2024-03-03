c2e6959b04a2f31467c03ac2301226194e1d0863
Igor <81161065+KupiraIgor@users.noreply.github.com>
2024-03-03 14:55:07 +0200

    added title head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,7 @@ export default defineNuxtConfig({
         name: 'viewport',
         content: 'width=device-width, initial-scale=1, maximum-scale=1',
       },
+      { title: 'TestTask' },
     ],
   },
 });
