b8aa498e9c7fe97637840e963ebf4da4d8c5f4d0
Igor <81161065+KupiraIgor@users.noreply.github.com>
2024-03-10 18:04:32 +0200

    Fixed text-overflow

--- components/Base/ItemUser.vue
+++ components/Base/ItemUser.vue
@@ -12,10 +12,18 @@ const props = defineProps({
 <template>
   <div class="item-user">
     <img class="item-user__img" :src="user.photo" alt="" />
-    <div class="item-user__name" :title="user.name">{{ user.name }}</div>
-    <div class="item-user__pos" :title="user.position">{{ user.position }}</div>
-    <a class="item-user__mail" :href="`mailto:${user.email}`" :title="user.email">{{ user.email }}</a>
-    <a class="item-user__phone" :href="`tel:${user.phone}`" :title="user.phone">{{ user.phone }}</a>
+    <div class="item-user__name" :title="user.name">
+      <span>{{ user.name }}</span>
+    </div>
+    <div class="item-user__pos" :title="user.position">
+      <span>{{ user.position }}</span>
+    </div>
+    <a class="item-user__mail" :href="`mailto:${user.email}`" :title="user.email">
+      <span>{{ user.email }}</span>
+    </a>
+    <a class="item-user__phone" :href="`tel:${user.phone}`" :title="user.phone">
+      <span>{{ user.phone }}</span>
+    </a>
   </div>
 </template>
 
@@ -41,13 +49,25 @@ const props = defineProps({
   }
 
   &__name,
-  &__pos,
   &__mail,
+  &__pos,
   &__phone {
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 100%;
+    position: relative;
+    width: 100%;
+    min-height: 2.6rem;
+
+    span {
+      position: absolute;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: 100%;
+      text-align: center;
+    }
   }
 }
 </style>
